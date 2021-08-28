da4853ec512eec0be5eb7986591bbf7e530c700b
FonDitbul <niki7084@naver.com>
2021-08-28 17:31:05 +0900

    signup 2차 비밀번호 예외처리

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 const users = require('../models/userDatabase');
+const passport = require('../controll/passport')
 
 const shortid = require('shortid')
 const getDate = require('../util/date')
@@ -24,15 +25,16 @@ router.get('/logout', (request,response)=>{
 
 
 router.post('/signup', (request,response)=>{
-  // console.log(request.body);
-  const newUser = new users(request.body);
-  newUser.id = shortid.generate();
-  newUser.signDate = getDate();
-      newUser.save((err)=>{
+    if(request.body.password !== request.body.passwordConfirm){
+        return response.send('Incorrect confirm password !')
+    }
+    const newUser = new users(request.body);
+    newUser.id = shortid.generate();
+    newUser.signDate = getDate();
+    newUser.save((err)=>{
         if(err) return response.status(500).json({message: 'failure save'})
         else return response.status(200).json({message:'save success'})
-      })
-  // response.send(request.body)
+    })
 })
 
 module.exports = router;
