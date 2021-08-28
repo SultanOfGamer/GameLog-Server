f98b77498ca09f1e7d15d35fa4518c828bb10bff
FonDitbul <niki7084@naver.com>
2021-08-28 16:37:00 +0900

    날짜 관련 함수 파일 분리

---
+++ util/date.js
@@ -0,0 +1,18 @@
+function getDate(){
+    const date = new Date();
+    var year = date.getFullYear().toString();
+
+    var month = date.getMonth() + 1;
+    month = month < 10 ? '0' + month.toString() : month.toString();
+
+    var day = date.getDate();
+    day = day < 10 ? '0' + day.toString() : day.toString();
+
+    let hours = date.getHours(); // 시
+    let minutes = date.getMinutes();  // 분
+    let seconds = date.getSeconds();  // 초
+
+    return year + '/' +  month + '/' + day + '-' + hours + ':' + minutes + ':' + seconds;
+}
+
+module.exports = getDate;
