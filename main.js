4dd7074e7208536941269dcb955fba6b2e0c1d17
FonDitbul <niki7084@naver.com>
2021-08-28 17:23:56 +0900

    mongodb session store 추가

--- main.js
+++ main.js
@@ -8,21 +8,34 @@ const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 const session = require('express-session')
 const bodyParser = require('body-parser')
-const axios = require('axios')
+// const axios = require('axios')
 const compression = require('compression')
 const helmet = require('helmet')
-const shortid = require('shortid')
+
+const MongoStore = require('connect-mongo');
+const mongoose = require("mongoose");
 
 const express = require('express');
 const app = express();
-
-const indexRouter = require('./routes/index')
-const authRouter = require('./routes/auth');
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(compression())
 app.use(helmet());
+
+app.use(
+    session({
+        secret: "rqwr@#^^#fsazfcz",
+        saveUninitialized: false, // don't create session until something stored
+        resave: false, //don't save session if unmodified
+        store: MongoStore.create({
+            mongoUrl:"mongodb://127.0.0.1:27017/testserver"
+        }),
+    })
+)
+
+const indexRouter = require('./routes/index')
+const authRouter = require('./routes/auth');
 
 app.use('/', indexRouter)
 app.use('/auth', authRouter);
@@ -31,16 +44,16 @@ app.use('/auth', authRouter);
 
 // ERROR handling
 app.use(function(request, response, next){
-  response.status(404).send('Sorry cant find that!');
+    response.status(404).send('Sorry cant find that!');
 })
 app.use(function (err, request, response, next) {
-  console.log(err)
-  if(response.headerSent){
-    return next(err);
-  }
-  response.status(500).send('500 ERROR !!')
+    console.log(err)
+    if(response.headerSent){
+        return next(err);
+    }
+    response.status(500).send('500 ERROR !!')
 });
 
 app.listen(3000, ()=>{
-  console.log('listen express server!')
+    console.log('listen express server!')
 })
