f179ce84188bacedddf5e8feb8529094485992e6
FonDitbul <niki7084@naver.com>
2021-08-29 23:21:12 +0900

    gameModes 네이밍 및 파일변경

--- models/gameGameModes.js
+++ models/gameGameModes.js
@@ -1,32 +1,23 @@
+
 const mongoose = require('./initDB')
+
 
 const gameGameModesSchema = new mongoose.Schema({
     id:{type:String, unique: true},
-    name:{type:String},
+    name:{type:String, unique: true},
     slug:{type:String},
-    alternative_name:{type:String},
     created_at:{type:String},
     updated_at:{type:String},
-    category_id:{type:String},
-    category_name:{type:String}
 })
 
-const gameGameModesCategory = {
-    1: "console",
-    2: "arcade",
-    3: "platform",
-    4: "operating_system",
-    5: "portable_console",
-    6: "computer"
-}
-
 const axios = require('axios')
-const gameGameModes = mongoose.model('gameGameModes', gameGameModesSchema);
+const gameGameModes = mongoose.model('game_GameModes', gameGameModesSchema);
+// gameCategory.collection.createIndex({id:1},{unique:true})
 
 const IGDBconfig = require('../config/IGDBconfig.json')
 
 const response = axios({
-    url: "https://api.igdb.com/v4/platforms",
+    url: "https://api.igdb.com/v4/game_modes",
     method: 'POST',
     headers: {
         'Accept': IGDBconfig.IGDB.Accept,
@@ -36,24 +27,18 @@ const response = axios({
     data: "fields *; limit 50;"
 })
     .then(response => {
-        // return response.data
         const resultData = response.data;
         resultData.forEach(i => {
-            const gameGameModesDB = new gameGameModes(i);
-            gameGameModesDB.category_id = i.category
-            gameGameModesDB.category_name = gameGameModesCategory[i.category]
-            gameGameModesDB.save((err) => {
+            const gameGameModesInstance = new gameGameModes(i);
+            gameGameModesInstance.save((err) => {
                 if (err) return 'err'
                 else return 'save complete'
             })
         })
-        gameGameModes.find({}, function(err, game){
-            // console.log(game)
-        })
     })
     .catch(err => {
         console.error(err);
-        console.log('err')
+        console.log('err dad')
     });
 
-// gameGameModes
+
