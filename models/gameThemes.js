d817875b4762de008cea227827ba7aa8b7f1ca2b
FonDitbul <niki7084@naver.com>
2021-08-29 23:27:11 +0900

    Themes DB 구현

---
+++ models/gameThemes.js
@@ -0,0 +1,45 @@
+const mongoose = require('./initDB')
+
+const gameThemesSchema = new mongoose.Schema({
+    id:{type:String, unique: true},
+    name:{type:String, unique: true},
+    slug:{type:String, unique: true},
+    created_at:{type:String},
+    updated_at:{type:String},
+})
+
+const axios = require('axios')
+const gameThemes = mongoose.model('game_Themes', gameThemesSchema);
+// gameCategory.collection.createIndex({id:1},{unique:true})
+
+const IGDBconfig = require('../config/IGDBconfig.json')
+
+const response = axios({
+    url: "https://api.igdb.com/v4/themes",
+    method: 'POST',
+    headers: {
+        'Accept': IGDBconfig.IGDB.Accept,
+        'Client-ID': IGDBconfig.IGDB.Client_ID,
+        'Authorization': IGDBconfig.IGDB.Authorization,
+    },
+    data: "fields *; limit 50;"
+})
+    .then(response => {
+        const resultData = response.data;
+        resultData.forEach(i => {
+            const gameThemesInstance = new gameThemes(i);
+            gameThemesInstance.save((err) => {
+                if (err) return 'err'
+                else return 'save complete'
+            })
+        })
+    })
+    .catch(err => {
+        console.error(err);
+        console.log('err dad')
+    });
+
+module.exports={
+    getGameThemesDB: gameThemes
+}
+
