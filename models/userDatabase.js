29c712bcd69054b08026e453d20deeef788c450f
FonDitbul <niki7084@naver.com>
2021-09-02 11:55:19 +0900

    sign up 스키마 변경

--- models/userDatabase.js
+++ models/userDatabase.js
@@ -2,9 +2,9 @@ const mongoose = require('./initDB')
 
 const userSchema = new mongoose.Schema({
   id:{type:String, unique: true, },
-  email:{type:String, unique: true, index:true},
-  nickname:{type:String, unique: true},
-  password:String,
+  email:{type:String, unique: true, index:true, required:true},
+  nickname:{type:String, unique: true, required:true},
+  password:{type:String, required:true},
   signDate:String
 })
 
