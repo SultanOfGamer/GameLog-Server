b4585228c97009f37dfa9271499c2290233b6fc8
FonDitbul <niki7084@naver.com>
2021-09-02 14:51:23 +0900

    이메일 유효성 검사 message 변경

--- routes/auth.js
+++ routes/auth.js
@@ -56,24 +56,26 @@ module.exports = function(passport){
     })
 
     router.post('/validation/:value', (request,response)=>{
-        // console.log(request.params.value)
         const value = request.params.value
         const queryString = request.query.value;
-        // console.log(queryString)
         switch(value){
             case 'email':
-                if(!userControl.emailValidation(queryString)) return response.send(false)
+                if(!userControl.emailValidation(queryString)) return response.send({message:"이메일 형식이 아닙니다."})
                 users.findOne({email:queryString}, function(err, user){
                     if(err) response.send(err)
-                    if(!user) response.send(true)
-                    else response.send(false)
+                    if(!user) response.send({data:true,
+                        message: "사용 가능한 이메일입니다."
+                    })
+                    else response.send({message:"이미 중복된 이메일이 존재합니다."})
                 })
                 break
             case 'nickname':
                 users.findOne({nickname:queryString}, function(err, user){
                     if(err) response.send(err)
-                    if(!user) response.send(true)
-                    else response.send(false)
+                    if(!user) response.send({data:true,
+                        message: "사용 가능한 닉네임 입니다."
+                    })
+                    else response.send({message:"이미 중복된 닉네임이 존재합니다."})
                 })
                 break
             default:
