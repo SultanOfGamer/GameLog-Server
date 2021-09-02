8e5f96be56474d243602e42bb48f8c4f492f3133
FonDitbul <niki7084@naver.com>
2021-09-02 11:49:28 +0900

    get AlterNames 파일 추가

---
+++ models/gameAlterNames.js
@@ -0,0 +1,51 @@
+const mongoose = require('./initDB')
+
+const gameAlterNamesSchema = new mongoose.Schema({
+    id:{type:String,unique:true},
+    name:{type:String},
+    game:{type:String},
+    comment:{type:String},
+})
+
+
+const axios = require('axios')
+const gameAlterNames = mongoose.model('game_alternames', gameAlterNamesSchema);
+
+const IGDBconfig = require('../config/IGDBconfig.json')
+function saveMongoDBdata(){
+
+}
+//TODO offset 설정, 모두 저장
+const response = axios({
+    url: "https://api.igdb.com/v4/alternative_names",
+    method: 'POST',
+    headers: {
+        'Accept': IGDBconfig.IGDB.Accept,
+        'Client-ID': IGDBconfig.IGDB.Client_ID,
+        'Authorization': IGDBconfig.IGDB.Authorization,
+    },
+    data: "fields *; limit 100;"
+})
+    .then(response => {
+        console.log(response)
+        const resultData = response.data;
+        resultData.forEach(i => {
+            const gameAlterNameInstance = new gameAlterNames(i);
+            gameAlterNameInstance.save((err) => {
+                if (err) return 'err'
+                else return 'save complete'
+            })
+        })
+        // gamePlatforms.find({}, function(err, game){
+        //     // console.log(game)
+        // })
+    })
+    .catch(err => {
+        console.error(err);
+        console.log('err')
+    });
+
+
+module.exports={
+    getGameAlterNames:gameAlterNames
+}
