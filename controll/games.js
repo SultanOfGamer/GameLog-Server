06f4cf3a183d164a71bf9a2fa62beb3143d2a1f2
FonDitbul <niki7084@naver.com>
2021-09-05 21:00:02 +0900

    games control 함수 추가

--- controll/games.js
+++ controll/games.js
@@ -3,28 +3,38 @@
 
 
 module.exports = {
-    getGame:function(model){// 조건 argument 설정
+    getGame:function(model){// init data
         const response = model.find()
-            .where('aggregated_rating').gte(97)
+            .where('aggregated_rating').gte(95)
             .where('aggregated_rating_count').gt(5)
             .sort('aggregated_rating')
+            .limit(10)
             .then(data=>{
                 return data
             })
         return response
     },
-    getGameOptions:function(model, id){
-        const tempGame = model.find({id:id},function(err,data){
-            if(err) return 'err'
-            console.log(data)
-        })
-            // .then(data=>{
-            //     console.log(data)
-            // })
+    getCategory:function(type, model){ // 테마, 장르 보내기, 각 별로 게임 불러오기
+        let response;
+        switch(type){
+            case 'genres':
+                response = model.find()
+                    .limit(30)
+                    .then(data=>{
+                        return data
+                    })
+                break
+            case 'themes':
+                response = model.find()
+                    .limit(30)
+                    .then(data=>{
+                        return data
+                    })
+                break
+        }
+        return response
     },
-    tansferGameList:function(){
-
-        this.getGame(model)
+    getGenres:function(model){
 
     },
     updateGame: function (  ) {
