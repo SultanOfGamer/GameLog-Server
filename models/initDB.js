28b3e55feb7bf01a0f8a61de74b63ba12fef4945
FonDitbul <niki7084@naver.com>
2021-09-06 23:06:44 +0900

    DB 접속

--- models/initDB.js
+++ models/initDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://127.0.0.1:27017';
+const url = 'mongodb://127.0.0.1:27017/testserver';
 const db = mongoose.connect(url, {
   authSource:"admin",
   auth:{
