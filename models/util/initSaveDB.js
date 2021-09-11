d884813d241b6cdde387cc3e68c68c3440c090c0
FonDitbul <niki7084@naver.com>
2021-09-11 16:42:46 +0900

    initsave 버그 수정

--- models/util/initSaveDB.js
+++ models/util/initSaveDB.js
@@ -1,17 +1,17 @@
 
 // 서버 구축시 실행하는 파일, database 구축
-const initDB = require('./initDB')
+const initDB = require('../initDB')
 
 
-const gameGameModes = require('./gameGameModes')
-const gameGenres = require('./gameGenresDB')
-const gamePlatforms = require('./gamePlatforms')
-const gameThemes = require('./gameThemes')
-const gameList = require('./gamesDB')
+const gameGameModes = require('../gameGameModes')
+const gameGenres = require('../gameGenresDB')
+const gamePlatforms = require('../gamePlatforms')
+const gameThemes = require('../gameThemes')
+const gameList = require('../gamesDB')
 
 gameGameModes.saveGameModesDB();
 gameGenres.saveGenresDB();
 gamePlatforms.savePlatformsDB();
-gameThemes.savePlatformsDB();
+gameThemes.saveThemesDB();
 
 gameList.saveGameList();
