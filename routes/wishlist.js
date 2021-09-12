8192e5e9458bb1aa3afa7c541ab4c84d22e4a825
FonDitbul <niki7084@naver.com>
2021-09-12 16:38:01 +0900

    wish list insert 에러처리

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -6,20 +6,34 @@ const userControl = require('../controll/index').users
 const userGameControl = require('../controll/index').userGames;
 
 
+//user wishlist get router
+//로그인 정보
+//query page
 router.get('/', async (request,response)=>{
     if(userControl.isUser(request,response)) {
         let page = request.query.page - 1 // pagination
-        const wishlistData = await userGameControl.getUserWishGames(request.user, request.body, page)
-        response.send(wishlistData)
+        try{
+            const wishlistData = await userGameControl.getUserWishGames(request.user, request.body, page)
+            response.send(wishlistData)
+        }catch(err){
+            response.send(err)
+        }
     }else{
         response.send({message:'please login!'})
     }
 })
 
+//wish list insert
+//로그인정보
+// library insert와 duplicated code
 router.post('/insert', (request,response)=>{
     if(userControl.isUser(request,response)) {
         userGameControl.insertUserGames(request.user, request.body)
-        response.send({message:'insert success'})
+            .then(()=>{
+                return response.send({message:'insert success'})
+            }).catch((err)=> {
+            return response.send({message: 'insert fail', err: err})
+        })
     }else{
         response.send({message:'please login!'})
     }
