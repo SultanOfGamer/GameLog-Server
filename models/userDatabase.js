606336dea70c2b5a4b5e0f233f763f76f32f20da
FonDitbul <niki7084@naver.com>
2021-09-12 13:53:01 +0900

    user autoincrement index

--- models/userDatabase.js
+++ models/userDatabase.js
@@ -1,12 +1,15 @@
 const mongoose = require('./initDB')
+const autoIncrementIndex = require('mongoose-sequence')(mongoose);
 
 const userSchema = new mongoose.Schema({
-  id:{type:String, unique: true},
+  // id:{type:String, unique: true},
   email:{type:String, unique: true, index:true, required:true},
   nickname:{type:String, unique: true, required:true},
   password:{type:String, required:true},
   signDate:String
 })
+
+userSchema.plugin(autoIncrementIndex, {id:"user_seq",inc_field:'id'});
 
 const users = mongoose.model('users', userSchema);
 
