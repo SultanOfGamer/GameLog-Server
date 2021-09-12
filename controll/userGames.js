7f9d64291f13258f8d947047e8ba6e6be39bf148
FonDitbul <niki7084@naver.com>
2021-09-12 16:38:46 +0900

    userGames controll 에러처리 추가

--- controll/userGames.js
+++ controll/userGames.js
@@ -8,72 +8,75 @@ module.exports = {
     getUserGames:function(user, offset){ //get user 별 library 데이터 베이스
         const pageCount = 30;
         return new Promise(function(resolve, reject){
-            userGameModel.find({userNickname:user.nickname})
+            userGameModel.find({userNickname:user.nickname, userGameStatus:{$ne:'wish'}}) //wish 제외 get
                 .limit(pageCount)
                 .skip(offset * pageCount)
                 .then(data=>{
                     resolve(data)
+                }).catch(err=>{
+                    reject(err)
                 })
         })
     },
     insertUserGames:function(user, body){ // add user 별 library 데이터 추가
-        let tempGame = new Promise(function(resolve, reject){
+        return new Promise(function(resolve, reject){
             gameList.find({id:body.gameId})
                 .limit(1)
-                .then(data=>{
-                    resolve(data)
-                    // console.log(data)
-                })
-        })
-        tempGame.then(data=>{
-            const tempGame = data[0]
+                .exec(function(err, data){
+                    if(err) reject(err)
+                    const tempGame = data[0]
 
-            userGameModel.create({
-                userEmail: user.email,
-                userNickname: user.nickname,
+                    userGameModel.create({
+                        userEmail: user.email,
+                        userNickname: user.nickname,
 
-                // game list 에서 받아온 game 정보 저장
-                gameId: tempGame.id,
-                gameName: tempGame.name,
-                aggregated_rating: tempGame.aggregated_rating,
-                aggregated_rating_count: tempGame.aggregated_rating_count,
-                first_release_date: tempGame.first_release_date,
+                        // game list 에서 받아온 game 정보 저장
+                        gameId: tempGame.id,
+                        gameName: tempGame.name,
+                        aggregated_rating: tempGame.aggregated_rating,
+                        aggregated_rating_count: tempGame.aggregated_rating_count,
+                        first_release_date: tempGame.first_release_date,
 
-                //유저측, 전송되는 데이터, request body로 받을 것
-                userGameEval: body.userGameEval,
-                userGameEvalText: body.userGameEvalText,
-                userGameMemo: body.userGameMemo,
-                userGameStatus: body.userGameStatus,
-                //정보가 저장된 시점
-                createdTime: getDate()
-            }).then(r =>{
-                return r
-            })
-                .catch((err)=>{
-                    // console.log(err)
-                    return err
+                        //유저측, 전송되는 데이터, request body로 받을 것
+                        userGameEval: body.userGameEval,
+                        userGameEvalText: body.userGameEvalText,
+                        userGameMemo: body.userGameMemo,
+                        userGameStatus: body.userGameStatus,
+                        //정보가 저장된 시점
+                        createdTime: getDate()
+                    }, function (err, game) {
+                        if (err) reject(err)
+                        resolve(game)
+                    })
                 })
         })
     },
     updateUserGames:function(user, body){
-        return userGameModel.findOneAndUpdate(
-            {id:body.id},
-            {
+        return new Promise((resolve, reject) => {
+            userGameModel.findOneAndUpdate(
+                {id:body.id},
+                {
                     userGameEval: body.userGameEval,
                     userGameEvalText: body.userGameEvalText,
                     userGameMemo: body.userGameMemo,
                     userGameStatus: body.userGameStatus,
                     updatedTime:getDate()
                 },{new:true},
-            (err, game)=>{
-                if(err) return err
-            }
-        ).catch((err)=>{console.log(err)})
+                (err, game)=>{
+                    if(err) reject(err)
+                    if(!game) resolve({message:'update fail not exist data'})
+                    resolve({message:'update success!'})
+                }
+            )
+        })
     },
     deleteUserGames:function(body){
-        return userGameModel.deleteOne({id:body.id}, function(err, result){
-            if(err){return err}
-            else return result
+        return new Promise((resolve, reject) => {
+            userGameModel.deleteOne({id:body.id}, function(err, result){
+                if(err) reject({message:'delete fail', err:err})
+                if(result.deletedCount == 0) resolve({message: 'delete fail'})
+                resolve({message: 'delete success'})
+            })
         })
     },
 
@@ -86,7 +89,9 @@ module.exports = {
                 .skip(page * pageCount)
                 .then(data=>{
                     resolve(data)
-                })
+                }).catch(err=>{
+                    reject(err)
+            })
         })
     }
 }
