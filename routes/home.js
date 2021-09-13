576bc7a038e20c74480a989c6c65d785b9d71ec4
FonDitbul <niki7084@naver.com>
2021-09-13 22:12:03 +0900

    로그인 x 일시

--- routes/home.js
+++ routes/home.js
@@ -31,10 +31,31 @@ router.get('/', async (request,response)=>{
             response.json(err)
         }
     }else{ //로그인 X, 첫 페이지 데이터 전송
-        gameControl.getGame()
-            .then(data=>{
-                response.json(data)
+         try{
+            //genresData를 user genres로 편의성 맞게 변경
+            const genresData = await gameControl.getCategory('genres')
+
+            const tempPromise = genresData.map(async function(genres){
+                return new Promise(function(resolve){
+                    gameControl.getGame(genres.name)
+                        .then(r=>{
+                            let tempDict = {};
+                            tempDict['genres'] = genres.name;
+                            tempDict['gameList'] = r;
+                            resolve(tempDict)
+                        })
+                })
             })
+            Promise.all(tempPromise).then(r=>{
+                response.send(r)
+            })
+        }catch(err){
+            response.json(err)
+        }
+        // gameControl.getGame()
+        //     .then(data=>{
+        //         response.json(data)
+        //     })
     }
 })
 
