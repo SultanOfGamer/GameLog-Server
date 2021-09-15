964b7ad7d42f69d7ec81b1b9dfa6ad062baa6778
FonDitbul <niki7084@naver.com>
2021-09-15 20:33:58 +0900

    user profile image name user id 지정

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -3,22 +3,24 @@ const router = express.Router();
 
 const multer = require('multer')
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: function(req, file, cb){ //storage path 지정
         cb(null, 'public/images/user_profile/')
     },
-    filename: function(req, file, cb){
-        cb(null, file.originalname )
+    filename: function(req, file, cb){ //file name 지정
+        cb(null, 'profile_user_id_'+req.user.id +'.' + file.originalname.split('.')[1] )
     }
 })
 const upload = multer({storage:storage,
-    limits:{file:5 * 1024 * 1024}, //5 MB 저장 제한
-    fileFilter:function (req, file, cb){
-        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
-            return cb(null, false, new Error('I don\'t have a clue!'));
-        }
-        cb(null, true);
-    }
+    limits:{file:1 * 1024 * 1024} //1 MB 저장 제한
+    // fileFilter:function (req, file, cb){
+    //     if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+    //         return cb(null, false, new Error('I don\'t have a clue!'));
+    //     }
+    //     cb(null, true);
+    // }
 }).single('image')
+
+
 const userProfile = require('../controll/index').userProfile
 
 
