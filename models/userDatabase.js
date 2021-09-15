760a32809c49a1c4d3e7557f37c26c0b4248eaeb
FonDitbul <niki7084@naver.com>
2021-09-15 17:46:59 +0900

    user profile image 스키마 구성

--- models/userDatabase.js
+++ models/userDatabase.js
@@ -1,12 +1,20 @@
 const mongoose = require('./initDB')
 const autoIncrementIndex = require('mongoose-sequence')(mongoose);
 
+
+const imageSchema = new mongoose.Schema({
+    height:{type:Number},
+    width:{type:Number},
+    url:{type:String, required:true},
+})
+
 const userSchema = new mongoose.Schema({
   // id:{type:String, unique: true},
   email:{type:String, unique: true, index:true, required:true},
   nickname:{type:String, unique: true, required:true},
   password:{type:String, required:true},
-  signDate:String
+  signDate:String,
+  profileImage:imageSchema
 })
 
 userSchema.plugin(autoIncrementIndex, {id:"user_seq",inc_field:'id'});
