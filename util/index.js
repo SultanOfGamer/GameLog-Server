1a7e8513f0c9067e328fa5e35a73dd55610ad734
FonDitbul <niki7084@naver.com>
2021-09-22 23:26:57 +0900

    translate index

--- util/index.js
+++ util/index.js
@@ -2,7 +2,10 @@
 const recommandSystem = require('./recommandML')
 const date = require('./date')
 
+const translatekakao = require('./translate_kakao').translateKAKAO;
+
 module.exports = {
     date:date,
     // recommandSystem:recommandSystem
+    transToKorea:translatekakao
 }
--- util/translate_kakao.js
+++ util/translate_kakao.js
@@ -4,29 +4,31 @@ const kakaoConfig = require('../config/translate.json')
 
 const RESTAPIKEY = kakaoConfig.kakao.RESTAPIKEY;
 
-async function translateKAKAO (query_test){
-    const query = '"It is a new beginning for Kratos. Living as a man, outside the shadow of the gods, he seeks solitude in the unfamiliar lands of Norse mythology. With new purpose and his son at his side, Kratos must fight for survival as powerful forces threaten to disrupt the new life he has created..."'
-    const api_url = 'https://dapi.kakao.com/v2/translation/translate';
-    const response = await axios(
-        {
-            method: 'post',
-            url:api_url,
-            headers: {
-                'Content-Type':"application/x-www-form-urlencoded",
-                'Authorization': "KakaoAK " + RESTAPIKEY,
-            },
-            data:`src_lang=en&target_lang=kr&query=${query}`
-        }
-    )
-        .then(response =>{
-            console.log(response.data)
-            console.log(response.data.translated_text[0])
-        }).catch(err=>{
-            console.log(err)
-        })
-    return response
+async function translateKAKAO (query){
+    return new Promise(function(resolve, reject){
+        const api_url = 'https://dapi.kakao.com/v2/translation/translate';
+        const response = axios(
+            {
+                method: 'post',
+                url:api_url,
+                headers: {
+                    'Content-Type':"application/x-www-form-urlencoded",
+                    'Authorization': "KakaoAK " + RESTAPIKEY,
+                },
+                data:`src_lang=en&target_lang=kr&query=${query}`
+            })
+            .then(response =>{
+                console.log(response.data)
+                console.log(query)
+                // console.log(response.data.translated_text[0])
+                resolve(response.data.translated_text[0])
+            }).catch(err=>{
+                console.log(err)
+                reject(err)
+            })
+    })
 }
 
 module.exports = {
-    translateKAKAO:translateKAKAO()
+    translateKAKAO:translateKAKAO
 }
