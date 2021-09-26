9a02ad67418f026527c075a5efcefb5689e90fb1
FonDitbul <niki7084@naver.com>
2021-09-26 23:37:18 +0900

    gameSearch get Search result 함수 추가

--- controll/gameSearch.js
+++ controll/gameSearch.js
@@ -1,10 +1,30 @@
 
 const gameList = require('../models/index').getGameList;
 
-
+async function getSearchResult(name, offset){
+    const pageCount = 30;
+    const string = name;
+    const reg = new RegExp(string, 'i')
+    return new Promise(function(resolve, reject){
+        gameList.find({name:{$regex:name, $options: "i"}},{name:1})
+            .limit(pageCount)
+            .skip(offset * pageCount)
+            .then(data=>{
+                resolve(data)
+            })
+            .catch(err=>{
+                reject(err)
+            })
+    })
+}
 
 module.exports={
     temp:function(){
 
     }
+}
+
+module.exports={
+    getSearchResult:getSearchResult,
+    getAlterSearch:getAlterSearch,
 }
