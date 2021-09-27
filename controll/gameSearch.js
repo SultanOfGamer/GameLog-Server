565749a5eb19b6289ec4bd8af4acb798474f1b8f
FonDitbul <niki7084@naver.com>
2021-09-27 22:30:12 +0900

    function 변경, 수정

--- controll/gameSearch.js
+++ controll/gameSearch.js
@@ -1,6 +1,6 @@
 
 const gameList = require('../models/index').getGameList;
-const userGame = require('../models/index').getUserGames;
+// const userGame = require('../models/index').getUserGames;
 
 async function getSearchResult(name){
     const pageCount = 10;
@@ -20,8 +20,6 @@ async function getSearchResult(name){
     })
 }
 
-//위의 결과와 밑의 결과의 중복x 합쳐서 전송하는 방법을 고안
-
 function getAlterSearch(name){
     const pageCount = 10;
     return new Promise(function(resolve, reject){
@@ -36,33 +34,8 @@ function getAlterSearch(name){
     })
 }
 
-function checkSelectedGame(user, gameId){
-    return new Promise(function (resolve, reject){
-        userGame.findOne({userid:user.id, gameId:gameId})
-            .then(data=>{
-                resolve(data)
-            })
-            .catch(err=>{
-                reject(err)
-            })
-    })
-}
-
-function getSelectedGame({id}){
-    return new Promise(function(resolve, reject){
-        gameList.findOne({id:id})
-            .then(data=>{
-                resolve(data)
-            })
-            .catch(err=>{
-                reject(err)
-            })
-    })
-}
 module.exports={
     getSearchResult:getSearchResult,
     getAlterSearch:getAlterSearch,
 
-    getSelectedGame:getSelectedGame,
-    checkSelectedGame:checkSelectedGame
 }
