d2609bc8202772dde06d245292568af418777a5f
FonDitbul <niki7084@naver.com>
2021-09-28 23:08:50 +0900

    init data file create

---
+++ routes/initdata.js
@@ -0,0 +1,35 @@
+//
+// 처음 보내줘야할 genres, themes 데이터
+
+//home과 user 회원 가입시에 등록하는 선호 분야 라우팅 파일 분리
+
+//Tab 별로 rotuer은 TAB 으로 구별
+const express = require('express');
+const router = express.Router();
+
+const gameControll = require('../controll/index').games;
+
+router.get('/genres', async (request, response)=>{
+    let sendObj = [];
+    gameControll.getCategory('genres',0, 23)
+        .then(data=>{
+            sendObj.push(data)
+        })
+        .then(()=>{
+            response.json(sendObj[0])
+        })
+})
+
+router.get('/themes', async (request, response)=>{
+    let sendObj = [];
+    gameControll.getCategory('themes', 0, 22)
+        .then(data=>{
+            sendObj.push(data)
+        })
+        .then(()=>{
+            response.json(sendObj[0])
+        })
+
+})
+
+module.exports = router
