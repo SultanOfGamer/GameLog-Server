5b22efded6f0345edab11fae80aec8f82fcbb5a2
FonDitbul <niki7084@naver.com>
2021-09-28 20:18:37 +0900

    body parser json

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 app.use(compression())
 app.use(helmet());
 app.use(express.static('public'))
