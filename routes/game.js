ad48971d1972b8328b5371c0fbd83409720c7f54
FonDitbul <niki7084@naver.com>
2021-09-28 21:36:04 +0900

    별점순, 이름순, 담은 순 sort

--- routes/game.js
+++ routes/game.js
@@ -4,24 +4,43 @@ const router = express.Router();
 const userControl = require('../controll/index').users
 const userGameControl = require('../controll/index').userGames;
 
-//TODO 쿼리별 데이터 받아오기 argument 수정
-//Library & Wishlist
 
-router.get('/:tabOption/get', async (request,response)=>{
-    const bar = request.params.tabOption;
+function checkInArrayString(arr, val) { //domain check value
+    return arr.some(arrVal => val === arrVal);
+}
+
+//Library & Wishlist
+router.get('/:tapbar/get', async (request,response)=>{
+    const tabBar = request.params.tapbar;
+
+    const sort = {}; // sort send value
+    const sortDomain = ['aggregated_rating', 'first_release_date', 'gameName', 'createdTime'] //{별점순, 출시일 순, abc순, 담은 순}
+    const sortTypeAsc = ['asc', 'desc'] // {asc, desc} 오름차순 내림차순
+
+    const sortReqType = request.query.sorttype;
+    const sortReq = request.query.sort
+
+    if(sortReqType === sortTypeAsc[0]) sort[sortReq] = 1; //오름차순
+    else if(sortReqType === sortTypeAsc[1]) sort[sortReq] = -1; //내림차순
+    else if(sortReqType === undefined) sort['createdTime'] = -1 //default sort값
+
+    //domain에 있는 sort값이 아닐 때 예외처리
+    if(sortTypeAsc.includes(sortReqType) === false || checkInArrayString(sortDomain, sortReq) === false)
+        response.json({message:'err in sort type'})
+
     if(userControl.isUser(request,response)) {
         let page = request.query.page - 1 // pagination
         try{
-            switch (bar){
+            switch (tabBar){
                 case 'library': // user library 전송
-                    const userLibraryList = await userGameControl.getUserGames(request.user, page)
+                    const userLibraryList = await userGameControl.getUserGames(request.user, page, sort)
                     response.json({
                         message:'success response',
                         data:userLibraryList
                     })
                     break;
                 case 'wishlist': //user wishlist 전송
-                    const wishlistData = await userGameControl.getUserWishGames(request.user, request.body, page)
+                    const wishlistData = await userGameControl.getUserWishGames(request.user, request.body, page, sort)
                     response.json({
                         message:'success response',
                         data:wishlistData
