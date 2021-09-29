f5e83ad1c0df1772ae95c9047a24bb9fedb8c347
FonDitbul <niki7084@naver.com>
2021-09-29 20:35:15 +0900

    random 로그인 X themes, genres 전송

--- routes/tabBarHome.js
+++ routes/tabBarHome.js
@@ -5,13 +5,14 @@ const router = express.Router();
 const userControl = require('../controll/index').users
 const gameControl = require('../controll/index').games;
 
+const shuffle = require('../util/index').shuffleArray
 
 //선택된 하나의 게임 detail 전송
 router.get('/select', async (request, response)=>{
     const gameId = request.query.gameId
     const user = request.user;
     let sendObj = {};
-    if(userControl.isUser(request,response)) {//game library 혹은 wish 정보 전송
+    if(userControl.isUser(request,response)) {//로그인 O game library 혹은 wish 정보 전송
         gameControl.checkSelectedGame(user, gameId)
             .then(data=>{
                 gameControl.getSelectedGame(gameId)
@@ -21,7 +22,7 @@ router.get('/select', async (request, response)=>{
                         response.json(sendObj)
                     })
             })
-    }else{ //game detail만
+    }else{ //로그인 X game detail만
         gameControl.getSelectedGame(gameId)
             .then(dataDetail=>{
                 sendObj['userGame'] = undefined;
@@ -38,17 +39,30 @@ router.get('/', async (request,response)=>{
         try{
             //TODO 추천 끝날 시 type 추천, 추가 전송!
             const genresData = await gameControl.getCategory('genres')
-            const tempPromise = genresData.map(async function(genres){
+            const themesData = await gameControl.getCategory('themes')
+            // const tempPromise = genresData.map(async function(genres){
+            //     return new Promise(function(resolve){
+            //         gameControl.getGameQuery('genres', genres.name)
+            //             .then(r=>{
+            //                 let tempObj = {};
+            //                 tempObj['type'] = genres.name;
+            //                 tempObj['game'] = r;
+            //                 resolve(tempObj)
+            //             })
+            //     })
+            // })
+            const tempPromise = themesData.map(async function(themes){
                 return new Promise(function(resolve){
-                    gameControl.getGameQuery(genres.name)
+                    gameControl.getGameQuery('themes', themes.name)
                         .then(r=>{
                             let tempObj = {};
-                            tempObj['type'] = genres.name;
+                            tempObj['type'] = themes.name;
                             tempObj['game'] = r;
                             resolve(tempObj)
                         })
                 })
             })
+
             Promise.all(tempPromise).then(r=>{
                 gameControl.getPopularGame()
                     .then(popularData=>{ // 유명 게임 포함 전송
@@ -61,24 +75,44 @@ router.get('/', async (request,response)=>{
         }
     }else{ //로그인 X, 첫 페이지 데이터 전송
         try{
-            //genresData를 user genres로 편의성 맞게 변경
-            let page = Math.floor(Math.random() * 5);
-            const genresData = await gameControl.getCategory('genres', page)
-
-            const tempPromise = genresData.map(async function(genres){
+            //TODO genres page가 4이하일때 themes를 불러올 필요 x
+            //themes data
+            let page2 = Math.floor(Math.random() * 11)
+            const themesData = await gameControl.getCategory('themes', page2, 2)
+            const tempThemesData = themesData.map(async function(category){
                 return new Promise(function(resolve){
-                    gameControl.getGame(genres.name)
+                    gameControl.getGame('themes', category.name)
                         .then(r=>{
                             let tempObj = {};
+                            tempObj['type'] = category.name;
+                            tempObj['game'] = r;
+                            resolve(tempObj)
+                        })
+                })
+            })
+            //genres default data
+            let page = Math.floor(Math.random() * 5);
+            let genresData = await gameControl.getCategory('genres', page)
+            let tempPromise = genresData.map(async function(genres){
+                return new Promise(function(resolve){
+                    gameControl.getGame('genres', genres.name)
+                        .then(r=>{
+                            let tempObj = {};
+                            //genres 이름
                             tempObj['type'] = genres.name;
                             tempObj['game'] = r;
                             resolve(tempObj)
                         })
                 })
             })
-            Promise.all(tempPromise).then(r=>{
+            let sendPromise = tempPromise;
+            //genres 데이터 개수가 3개 일때 themes 예외처리
+            if(page === 4) sendPromise = tempPromise.concat(tempThemesData)
+
+            Promise.all(sendPromise).then(r=>{
                 gameControl.getPopularGame()
                     .then(popularData=>{ // 유명 게임 포함 전송
+                        r = shuffle(r)
                         r.unshift({'type':'popular', 'game':popularData})
                         response.send(r)
                     })
--- util/index.js
+++ util/index.js
@@ -4,8 +4,19 @@ const date = require('./date')
 
 // const translatekakao = require('./translate_kakao').translateKAKAO;
 
+function shuffle(array){
+    for (let index = array.length - 1; index >0; index--){
+        const randomPosition = Math.floor(Math.random() * (index + 1));
+        const temparr = array[index];
+        array[index] = array[randomPosition];
+        array[randomPosition] = temparr
+    }
+    return array
+}
+
 module.exports = {
     date:date,
     recommandSystem:recommandSystem,
     // transToKorea:translatekakao
+    shuffleArray:shuffle
 }
