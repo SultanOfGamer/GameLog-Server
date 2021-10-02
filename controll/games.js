ae333b7ca8a4bb13b9120984065efa6ce5ac9437
FonDitbul <niki7084@naver.com>
2021-10-02 09:25:32 +0900

    getGame function argument 추가

--- controll/games.js
+++ controll/games.js
@@ -32,28 +32,53 @@ async function getSelectedGame(gameId){ // 선택된 게임의 detail 정보
 }
 
 module.exports = {
-    getGame:function(genres){// 로그인 X 유명 장르를 통하여 게임 받아오기
-        return new Promise(function(resolve){
-            gameGameList.find({genres:{$elemMatch:{name:genres}}}, {id:1,name:1, cover:1})
-                .select({_id:0})
-            .where('aggregated_rating').gte(4)
-            .where('aggregated_rating_count').gt(5)
-            .sort({'aggregated_rating_count':-1, 'aggregated_rating':-1})
-            .limit(10)
-            .then(data=>{
-                resolve(data)
+    getGame:function(type, genres){// 로그인 X 유명 장르를 통하여 게임 받아오기
+        if(type === 'genres'){
+            return new Promise(function(resolve){
+                gameGameList.find({genres:{$elemMatch:{name:genres}}}, {id:1,name:1, cover:1})
+                    .select({_id:0})
+                    .where('aggregated_rating').gte(4)
+                    .where('aggregated_rating_count').gt(5)
+                    .sort({'aggregated_rating_count':-1, 'aggregated_rating':-1})
+                    .limit(10)
+                    .then(data=>{
+                        resolve(data)
+                    })
             })
-        })
+        }else if(type === 'themes'){
+            return new Promise(function(resolve){
+                gameGameList.find({themes:{$elemMatch:{name:genres}}}, {id:1,name:1, cover:1})
+                    .select({_id:0})
+                    .where('aggregated_rating').gte(4)
+                    .where('aggregated_rating_count').gt(5)
+                    .sort({'aggregated_rating_count':-1, 'aggregated_rating':-1})
+                    .limit(10)
+                    .then(data=>{
+                        resolve(data)
+                    })
+            })
+        }
     },
-    getGameQuery:function(genres){ //user genres 속 데이터 받아오기
-        return new Promise(function(resolve){
-            gameGameList.find({genres:{$elemMatch:{name:genres}}}, {id:1,name:1, cover:1})
-                .select({_id:0})
-            .limit(5)
-            .then(data=>{
-                resolve(data)
+    getGameQuery:function(type, name){ //user genres & themes  속 데이터 받아오기
+        if(type === 'genres'){
+            return new Promise(function(resolve){
+                gameGameList.find({genres:{$elemMatch:{name:name}}}, {id:1,name:1, cover:1})
+                    .select({_id:0})
+                    .limit(10)
+                    .then(data=>{
+                        resolve(data)
+                    })
             })
-        })
+        }else if(type === 'themes'){
+            return new Promise(function(resolve){
+                gameGameList.find({themes:{$elemMatch:{name:name}}}, {id:1,name:1, cover:1})
+                    .select({_id:0})
+                    .limit(10)
+                    .then(data=>{
+                        resolve(data)
+                    })
+            })
+        }
     },
     getCategory:function(type, offset = 0, showNumArg = 5){ // 테마, 장르 보내기, 각 별로 게임 불러오기
         let response; // return 값
@@ -88,13 +113,13 @@ module.exports = {
         return new Promise(function(resolve){
             gameGameList.find({},{id:1,name:1, cover:1})
                 .select({_id:0})
-            .where('aggregated_rating').gte(4)
-            .where('aggregated_rating_count').gt(20)
-            .sort({'aggregated_rating_count':-1, 'aggregated_rating':-1})
-            .limit(10)
-            .then(data=>{
-                resolve(data)
-            })
+                .where('aggregated_rating').gte(4)
+                .where('aggregated_rating_count').gt(20)
+                .sort({'aggregated_rating_count':-1, 'aggregated_rating':-1})
+                .limit(10)
+                .then(data=>{
+                    resolve(data)
+                })
         })
     },
     getSelectedGame:getSelectedGame,
