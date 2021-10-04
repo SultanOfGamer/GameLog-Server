5d5eeba430fc7d1214e7b4357a2841a8bef75402
FonDitbul <niki7084@naver.com>
2021-10-04 14:09:06 +0900

    naming 변경

--- routes/tabBarSearch.js
+++ routes/tabBarSearch.js
@@ -11,43 +11,17 @@ const gameList = require('../models/index').getGameList;
 //id, name, cover 사진 전송
 router.get('/', async (request,response)=>{
     const gameName = request.query.name;
-    let send = {};
+    let sendObj = {};
 
     gameSearch.getSearchResult(gameName)
         .then(data=>{
-            send['game'] = data
+            sendObj['game'] = data
             gameSearch.getAlterSearch(gameName)
                 .then(dataAlter=>{
-                    send['game'] = dataAlter
-                    response.json(send)
+                    sendObj['game'] = dataAlter
+                    response.json(sendObj)
                 })
         })
 })
-
-
-
-//
-// router.get('/autotest',async (request, response)=>{
-//     try{
-//         let result = await gameList.aggregate([
-//             {
-//                 "$text":{
-//                     "autocomplete":{
-//                         "query":`${request.query.query}`,
-//                         "path":"name",
-//                         "fuzzy":{
-//                             "maxEdits":2,
-//                             "prefixLength":3
-//                         }
-//                     }
-//                 }
-//             }
-//         ]).toArray();
-//         response.send(result);
-//     }catch(err){
-//          response.status(500).send({ message: e.message });
-//     }
-//     //출처 : https://www.mongodb.com/developer/how-to/building-autocomplete-form-element-atlas-search-javascript/
-// })
 
 module.exports = router;
