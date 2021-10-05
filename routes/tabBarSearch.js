04769512286e4a6b9ab6d00e75647522937c3b7b
FonDitbul <niki7084@naver.com>
2021-10-05 16:56:32 +0900

    Search 리팩터링

--- routes/tabBarSearch.js
+++ routes/tabBarSearch.js
@@ -2,26 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
-const userControl = require('../controll/index').users
 const gameSearch = require('../controll/index').gameSearch
 
-const gameList = require('../models/index').getGameList;
-
 //search
-//id, name, cover 사진 전송
+//id, name, cover 전송
 router.get('/', async (request,response)=>{
-    const gameName = request.query.name;
+    const name = request.query.name;
     let sendObj = {};
 
-    gameSearch.getSearchResult(gameName)
-        .then(data=>{
-            sendObj['game'] = data
-            gameSearch.getAlterSearch(gameName)
-                .then(dataAlter=>{
-                    sendObj['game'] = dataAlter
-                    response.json(sendObj)
-                })
-        })
+    const gameName = await gameSearch.getSearchResult(name)
+    const alterName = await gameSearch.getAlterSearch(name)
+
+    sendObj = {gameName, alterName}
+    response.json(sendObj)
 })
 
 module.exports = router;
