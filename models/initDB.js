5dc740e441cb5fe1ee3290887b8717802bbe644c
FonDitbul <niki7084@naver.com>
2021-10-06 12:11:16 +0900

    mongo db connect 형식 변경

--- models/initDB.js
+++ models/initDB.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose')
 const url = 'mongodb://127.0.0.1:27017/gamelog';
-const db = mongoose.connect(url, {
-  authSource:"admin",
-  auth:{
-    username:"root",
-    password:"root"
-  },
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-},(err)=>{
-  if(err){
-    console.log(err.message);
-  }else{
-    console.log('mongodb success connect!')
-  }
-})
-
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const dbConnect = async () =>
+{
+  await mongoose.connect(url, {
+    authSource:"admin",
+    auth:{
+      username:"root",
+      password:"root"
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },(err)=>{
+  })
+}
+dbConnect()
+    .then(res=>console.log('mongodb success connect!!'))
+    .catch(err=>console.log(err))
 
 module.exports = mongoose;
