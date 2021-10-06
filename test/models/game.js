cbd4dcc767c9024059c94ecfde511f4ca335ec04
FonDitbul <niki7084@naver.com>
2021-10-06 12:10:45 +0900

    test model 파일 생성

---
+++ test/models/game.js
@@ -0,0 +1,214 @@
+const game = {
+    "id" : 999999,
+    "name" : "The Elder Scrolls V: Skyrim TestGame",
+    "slug" : "the-elder-scrolls-v-skyrim testgame",
+    "aggregated_rating" : 4.81,
+    "aggregated_rating_count" : 12,
+    "alternative_names" : [
+        {
+            "id" : 4222,
+            "name" : "TESV",
+        },
+        {
+            "id" : 9916,
+            "name" : "Skyrim",
+        },
+        {
+            "id" : 13759,
+            "name" : "TESV: Skyrim",
+        },
+        {
+            "id" : 13760,
+            "name" : "The Elder Scrolls 5: Skyrim",
+        },
+        {
+            "id" : 35673,
+            "name" : "TESV Skyrim",
+        },
+        {
+            "id" : 35674,
+            "name" : "TES5 Skyrim",
+        },
+        {
+            "id" : 35675,
+            "name" : "TES5: Skyrim",
+        },
+        {
+            "id" : 37431,
+            "name" : "TES5",
+        },
+        {
+            "id" : 37432,
+            "name" : "TES 5",
+        },
+        {
+            "id" : 37433,
+            "name" : "TES Skyrim",
+        },
+        {
+            "id" : 47147,
+            "name" : "The Elder Scrolls V: Skyrim Special Edition",
+        },
+        {
+            "id" : 47148,
+            "name" : "Dawnguard",
+        },
+        {
+            "id" : 47149,
+            "name" : "TES5",
+        },
+        {
+            "id" : 47150,
+            "name" : "The Elder Scrolls 5: Skyrim",
+        },
+        {
+            "id" : 47151,
+            "name" : "Skyrim",
+        },
+        {
+            "id" : 47152,
+            "name" : "TESV: Skyrim",
+        }
+    ],
+    "involved_companies" : [
+        {
+            "id" : 3719,
+            "company" : [
+                {
+                    "id" : 126,
+                    "name" : "Bethesda Game Studios",
+                }
+            ],
+        },
+        {
+            "id" : 95350,
+            "company" : [
+                {
+                    "id" : 16565,
+                    "name" : "Bethesda Softworks",
+                }
+            ],
+        }
+    ],
+    "first_release_date" : 1320883200,
+    "created_at" : 1301166960,
+    "updated_at" : 1633256030,
+    "release_dates" : [
+        21154,
+        21155,
+        21156,
+        21664,
+        21666,
+        106167,
+        106168,
+        106169,
+        282088
+    ],
+    "category" : "main_game",
+    "game_modes" : [
+        {
+            "id" : 1,
+            "name" : "Single player",
+        }
+    ],
+    "genres" : [
+        {
+            "id" : 12,
+            "name" : "Role-playing (RPG)",
+        },
+        {
+            "id" : 31,
+            "name" : "Adventure",
+        }
+    ],
+    "platforms" : [
+        {
+            "id" : 6,
+            "category" : 4,
+            "name" : "PC (Microsoft Windows)",
+            "category_name" : "operating_system"
+        },
+        {
+            "id" : 9,
+            "category" : 1,
+            "name" : "PlayStation 3",
+            "category_name" : "console"
+        },
+        {
+            "id" : 12,
+            "category" : 1,
+            "name" : "Xbox 360",
+            "category_name" : "console"
+        }
+    ],
+    "themes" : [
+        {
+            "id" : 1,
+            "name" : "Action",
+        },
+        {
+            "id" : 17,
+            "name" : "Fantasy",
+        },
+        {
+            "id" : 33,
+            "name" : "Sandbox",
+        },
+        {
+            "id" : 38,
+            "name" : "Open world",
+        }
+    ],
+    "storyline" : "The Empire of Tamriel is on the edge. The High King of Skyrim has been murdered. Alliances form as claims to the throne are made. In the midst of this conflict, a far more dangerous, ancient evil is awakened. Dragons, long lost to the passages of the Elder Scrolls, have returned to Tamriel. The future of Skyrim, even the Empire itself, hangs in the balance as they wait for the prophesized Dragonborn to come; a hero born with the power of The Voice, and the only one who can stand amongst the dragons.",
+    "summary" : "The next chapter in the highly anticipated Elder Scrolls saga arrives from the makers of the 2006 and 2008 Games of the Year, Bethesda Game Studios. Skyrim reimagines and revolutionizes the open-world fantasy epic, bringing to life a complete virtual world open for you to explore any way you choose.  \n \nPlay any type of character you can imagine, and do whatever you want; the legendary freedom of choice, storytelling, and adventure of The Elder Scrolls is realized like never before.  \n \nSkyrim’s new game engine brings to life a complete virtual world with rolling clouds, rugged mountains, bustling cities, lush fields, and ancient dungeons.  \n \nChoose from hundreds of weapons, spells, and abilities. The new character system allows you to play any way you want and define yourself through your actions.  \n \nBattle ancient dragons like you’ve never seen. As Dragonborn, learn their secrets and harness their power for yourself.",
+    "cover" : [
+        {
+            "id" : 85100,
+            "game" : 472,
+            "height" : 800,
+            "width" : 600,
+            "url" : "//images.igdb.com/igdb/image/upload/t_thumb/co1tnw.jpg",
+        }
+    ],
+    "screenshots" : [
+        {
+            "id" : 345,
+            "game" : 472,
+            "height" : 720,
+            "width" : 1280,
+            "url" : "//images.igdb.com/igdb/image/upload/t_thumb/muv70yw3rds1cw8ymr5v.jpg",
+        },
+        {
+            "id" : 346,
+            "game" : 472,
+            "height" : 720,
+            "width" : 1280,
+            "url" : "//images.igdb.com/igdb/image/upload/t_thumb/xzk2h41fiye7uwbhc6ub.jpg",
+        },
+        {
+            "id" : 347,
+            "game" : 472,
+            "height" : 720,
+            "width" : 1280,
+            "url" : "//images.igdb.com/igdb/image/upload/t_thumb/urqw7ltwmhr39gkidsih.jpg",
+        },
+        {
+            "id" : 348,
+            "game" : 472,
+            "height" : 720,
+            "width" : 1280,
+            "url" : "//images.igdb.com/igdb/image/upload/t_thumb/t0mus35qrgclafo1ql8k.jpg",
+        },
+        {
+            "id" : 349,
+            "game" : 472,
+            "height" : 720,
+            "width" : 1280,
+            "url" : "//images.igdb.com/igdb/image/upload/t_thumb/x5bbaqvgbpaz4hzlfeqb.jpg",
+        }
+    ],
+    "__v" : 0
+}
+
+
+module.exports = game;
---
+++ test/models/user.js
@@ -0,0 +1,14 @@
+const users = [
+    {
+        email:'testUser1@naver.com',
+        password:'123456'
+    },
+    {
+        email:'testUser2@naver.com',
+        password:'123456'
+    },
+    {
+        email:'testUser3@naver.com',
+        password:'123456'
+    },
+]
