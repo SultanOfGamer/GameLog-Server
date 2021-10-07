c514e843dff3b3455c271d961b48f0030d2e6c99
FonDitbul <niki7084@naver.com>
2021-10-07 21:42:59 +0900

    prefer temp data

---
+++ test/models/userPrefer.js
@@ -0,0 +1,31 @@
+const prefer = {
+    "prefer":[
+        {
+            "category": "themes",
+            "id": 21,
+            "name": "Survival"
+        },
+        {
+            "category":"themes",
+            "id":38,
+            "name":"Open world"
+        },
+        {
+            "category":"genres",
+            "id":15,
+            "name":"Strategy"
+        },
+        {
+            "category":"genres",
+            "id":12,
+            "name":"Role-playing (RPG)"
+        },
+        {
+            "category":"genres",
+            "id":31,
+            "name":"Adventure"
+        }
+    ]
+}
+
+module.exports = prefer;
