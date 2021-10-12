c24bcf3159eb7442783db442bc02b75dee180755
FonDitbul <niki7084@naver.com>
2021-10-12 23:59:56 +0900

    app test

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,18 @@
 const request = require('supertest')
 const APP = require('../main')
 
-describe("GameLog",() => {
+describe("GameLog", () => {
     let app = APP;
     it("GameLog", async () => {
         const res = await request(app)
             .get('/')
-        expect(res.statusCode).toEqual(200)
+        return expect(res.statusCode).toEqual(200)
     })
 
     it("GameLog 404", async() => {
-         const res = await request(app)
+        const res = await request(app)
             .get('/51251')
-        expect(res.statusCode).toEqual(404)
+        return expect(res.statusCode).toEqual(404)
     })
+
 })
