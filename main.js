e3a99e85c9cabbb4b82bf86528608f8d1051e1a9
FonDitbul <niki7084@naver.com>
2021-10-13 01:02:30 +0900

    mongo session url 수정

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ app.use(
         saveUninitialized: false, // don't create session until something stored
         resave: false, //don't save session if unmodified
         store: MongoStore.create({
-            mongoUrl:"mongodb://127.0.0.1:27017/testserver"
+            mongoUrl:"mongodb://127.0.0.1:27017/gamelog"
         }),
     })
 )
