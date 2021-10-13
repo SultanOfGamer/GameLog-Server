c5c430aff3fd2c398ef8d51285f21f83c3516a0d
FonDitbul <niki7084@naver.com>
2021-10-14 01:19:15 +0900

    search test

--- test/service/Search.test.js
+++ test/service/Search.test.js
@@ -1,9 +1,13 @@
 // const APP = require('../../main')
 const request = require('supertest')
 
+const gameSearch = require('../controll/index').gameSearch
+
 
 describe("Search TABBAR",() => {
-    it("search", () => {
+    it("search", async () => {
+        const gameName = await gameSearch.getSearchResult(name)
+        const alterName = await gameSearch.getAlterSearch(name)
 
     })
 })
