02105b4ac5ba6bd6db2a482cc01d2fe7992e667f
FonDitbul <niki7084@naver.com>
2021-10-20 18:50:31 +0900

    Search test

--- test/service/Search.test.js
+++ test/service/Search.test.js
@@ -1,14 +1,12 @@
-// const APP = require('../../main')
+const APP = require('../../main')
 const request = require('supertest')
-
-const gameSearch = require('../../controll/index').gameSearch
 
 
 describe("GET /search",() => {
-    it("search", async () => {
-        let name = 'the'
-        const gameName = await gameSearch.getSearchResult(name)
-        const alterName = await gameSearch.getAlterSearch(name)
-
+    test("search", async () => {
+        const name = 'the'
+        return request(APP)
+            .get(`/search?name=${name}`)
+            .expect(200)
     })
 })
