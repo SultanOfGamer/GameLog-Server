05a0f903f6497da699674b5bdba7cbb31d26a4e5
FonDitbul <niki7084@naver.com>
2021-10-20 18:54:59 +0900

    User 인증 파일 추가

---
+++ test/service/Auth.test.js
@@ -0,0 +1,29 @@
+const APP = require('../../main')
+const request = require('supertest')
+const user = require('../models/user').users;
+const errUser = require('../models/user').signUpErrUser;
+
+
+describe("user Validation", () => {
+    test('test', () => {
+
+    })
+})
+
+describe("login", () => {
+    test('test', () => {
+
+    })
+})
+
+describe("logout", () => {
+    test('login test', () => {
+
+    })
+})
+
+describe("logout", () => {
+    test('login test', () => {
+
+    })
+})
