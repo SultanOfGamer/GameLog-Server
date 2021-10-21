11b2c2200e30d7e34a2ee99e6613f4d7644a05d2
FonDitbul <niki7084@naver.com>
2021-10-21 21:29:58 +0900

    home test 코드

--- test/service/Home.test.js
+++ test/service/Home.test.js
@@ -1,36 +1,57 @@
 const APP = require('../../main')
+const user = require('../models/user').users;
+const prefer = require('../models/userPrefer');
+
 const request = require('supertest')
+const session = require('supertest-session')
+let testSession = null;
 
 
-
-describe("home test",() => {
-    let app = APP;
-
-    beforeEach(() => {
-
-    })
-    afterEach(() => {
-
-    })
-
-    it("Home", async () => {
-        const res = await request(app)
+describe("GET /",() => {
+    test("Login X",  () => {
+        return request(APP)
             .get('/')
-        expect(res.statusCode).toEqual(200)
+            .expect(200)
     })
+})
 
-    it("login Home", async() =>{
-        await request(app)
+describe("GET / Login User", () => {
+    beforeEach(async ()=>{
+        testSession = session(APP);
+
+        await request(APP)
+            .post('/auth/signup')
+            .send({
+                email:user[0].email,
+                nickname:user[0].nickname,
+                password:user[0].password,
+                passwordConfirm:user[0].password,
+            })
+        await testSession
             .post('/auth/login')
-            .send({email:"test@gmail.com",password:123456})
+            .send({
+                email:user[0].email,
+                password:user[0].password
+            })
+    })
+    afterEach(async() => {
+        await testSession
+            .delete('/profile/user')
+    })
 
-        const res = await request(app)
+    // test("Login O", async () =>{
+    //     await testSession
+    //         .put('/profile/category')
+    //         .send({prefer})
+    //
+    //     await testSession
+    //         .get('/')
+    //         .expect(200)
+    // })
+
+    test("login don't exist prefer", () => {
+        return testSession
             .get('/')
-        expect(res.statusCode).toEqual(200)
+            .expect(500)
     })
-
-    it("login don't exist prefer", async() => {
-
-    })
-
 })
