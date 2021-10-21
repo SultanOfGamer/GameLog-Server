65eb9459488666ab91d8712c9e93da15c5682e38
FonDitbul <niki7084@naver.com>
2021-10-21 16:02:44 +0900

    login 필요 서비스 에러 리팩터링

--- routes/tabBarLibAndWish.js
+++ routes/tabBarLibAndWish.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const logger = require('../config/wiston');
 
 const userControl = require('../controll/index').users
 const userGameControl = require('../controll/index').userGames;
-
 
 function checkInArrayString(arr, val) { //domain check value
     return arr.some(arrVal => val === arrVal);
 }
 
+router.use((request, response, next) => {
+    if(!(userControl.isUser(request, response))){
+        logger.error('로그인 필요')
+        response.send({
+            status:401,
+            message:'로그인이 필요합니다.'
+        })
+    }
+    next();
+})
+
 //Library & Wishlist
-router.get('/:tapbar', async (request,response)=>{
+router.get('/:tapbar', async (request,response,next)=>{
     const tabBar = request.params.tapbar;
 
     const sortObj = {}; // sort send value
@@ -28,61 +39,54 @@ router.get('/:tapbar', async (request,response)=>{
         if(sorttype !== undefined && sort !== undefined) //default 예외처리
             response.json({message:'err in sort type'})
 
-    if(userControl.isUser(request,response)) {
-        let page = request.query.page - 1 // pagination
-        try{
-            switch (tabBar){
-                case 'library': // user library 전송
-                    const userLibraryList = await userGameControl.getUserGames(request.user, page, sortObj)
-                    response.json({
-                        user:{
-                            id:request.user.id,
-                            email:request.user.email
-                        },
-                        message:'success response',
-                        data:userLibraryList
-                    })
-                    break;
-                case 'wishlist': //user wishlist 전송
-                    const wishlistData = await userGameControl.getUserWishGames(request.user, request.body, page, sortObj)
-                    response.json({
-                        user:{
-                            id:request.user.id,
-                            email:request.user.email
-                        },
-                        message:'success response',
-                        data:wishlistData
-                    })
-                    break;
-                default:
-                    response.json({message:'라우팅 확인!'})
-                    break;
-            }
-        }catch(err){
-            response.json({message:err})
+    let page = request.query.page - 1 // pagination
+    try{
+        switch (tabBar){
+            case 'library': // user library 전송
+                const userLibraryList = await userGameControl.getUserGames(request.user, page, sortObj)
+                response.json({
+                    user:{
+                        id:request.user.id,
+                        email:request.user.email
+                    },
+                    message:'success response',
+                    data:userLibraryList
+                })
+                break;
+            case 'wishlist': //user wishlist 전송
+                const wishlistData = await userGameControl.getUserWishGames(request.user, request.body, page, sortObj)
+                response.json({
+                    user:{
+                        id:request.user.id,
+                        email:request.user.email
+                    },
+                    message:'success response',
+                    data:wishlistData
+                })
+                break;
+            default:
+                next()
+                // response.json({message:'라우팅 확인!'})
+                break;
         }
-    }else{
-        response.json({message:'please login!'})
+    }catch(err){
+        response.json({message:err})
     }
 })
 
 //Library & Wishlist insert
 router.post('/:tapbar', (request,response)=>{
     const tabBar = request.params.tapbar;
-    if(userControl.isUser(request,response)) {
-        userGameControl.insertUserGames(request.user, request.body, tabBar)
-            .then((data)=>{
-                return response.json(
-                    {
-                        message:'insert success',
-                        data:data
-                    })
-            }).catch((err)=>{
-            return response.json({message:'insert fail', err:err})
-        })
-    }else{
-        response.json({message:'please login!'})
-    }
+    userGameControl.insertUserGames(request.user, request.body, tabBar)
+        .then((data)=>{
+            return response.json(
+                {
+                    message:'insert success',
+                    data:data
+                })
+        }).catch((err)=>{
+        return response.json({message:'insert fail', err:err})
+    })
 })
 
 //user library 데이터 업데이트
@@ -90,27 +94,19 @@ router.post('/:tapbar', (request,response)=>{
 // body -> 게임 id(number), 게임평가(number), 게임 평가(text), 게임 메모(text), 게임 status
 // body -> gameId, userGameEval, userGameEvalText, userGameMemo, userGameStatus 필요
 router.put('/:tapbar', async (request,response)=>{
-    if(userControl.isUser(request,response)) {
-        const sendMessage = await userGameControl.updateUserGames(request.user, request.body)
-            .catch((err)=> {
-                return response.json({message: 'update fail', err: err})
-            })
-        response.json(sendMessage)
-    }else{
-        response.json({message:'please login!'})
-    }
+    const sendMessage = await userGameControl.updateUserGames(request.user, request.body)
+        .catch((err)=> {
+            return response.json({message: 'update fail', err: err})
+        })
+    response.json(sendMessage)
 })
 
 //user library 데이터 삭제
 //삭제하는 id 전송후 삭제
 // null, 빈칸 입력 혹은 유저가 삭제했을 시
 router.delete('/:tapbar', async (request,response)=>{
-    if(userControl.isUser(request,response)) {
-        const sendMessage = await userGameControl.deleteUserGames(request.body)
-        response.json(sendMessage)
-    }else{
-        response.json({message:'please login!'})
-    }
+    const sendMessage = await userGameControl.deleteUserGames(request.body)
+    response.json(sendMessage)
 })
 
 module.exports = router;
