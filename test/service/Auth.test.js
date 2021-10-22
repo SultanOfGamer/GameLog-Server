d318fe652b10ae81ea456b025e77c8fde81d4860
FonDitbul <niki7084@naver.com>
2021-10-22 18:42:07 +0900

    describe 및 test 이름 수정

--- test/service/Auth.test.js
+++ test/service/Auth.test.js
@@ -6,7 +6,7 @@ const request = require('supertest')
 const session = require('supertest-session')
 let testSession = null;
 
-describe("POST /auth/login", () => {
+describe("POST /auth/login 로그인", () => {
     beforeAll(async () => {
         testSession = session(APP);
         await request(APP)
@@ -23,7 +23,7 @@ describe("POST /auth/login", () => {
             .delete('/profile/user')
     })
 
-    test('200 로그인 성공', async () => {
+    test('POST 200 / 로그인 성공', async () => {
         await testSession
             .post('/auth/login')
             .send({
@@ -32,7 +32,7 @@ describe("POST /auth/login", () => {
             })
             .expect(200)
     })
-    test('403 이메일 에러', async () => {
+    test('POST 403 / 이메일 에러', async () => {
         await testSession
             .post('/auth/login')
             .send({
@@ -41,7 +41,7 @@ describe("POST /auth/login", () => {
             })
             .expect(403)
     })
-    test('403 비밀번호 에러', async () => {
+    test('POST 403 / 비밀번호 에러', async () => {
         await testSession
             .post('/auth/login')
             .send({
@@ -52,7 +52,7 @@ describe("POST /auth/login", () => {
     })
 })
 
-describe("GET /auth/validation", () => {
+describe("GET /auth/validation 이메일, 닉네임 검사", () => {
     beforeAll(async () => {
         await request(APP)
             .post('/auth/signup')
@@ -77,44 +77,44 @@ describe("GET /auth/validation", () => {
             .delete('/profile/user')
     })
 
-    test('200 이메일 성공', () => {
+    test('GET 200 /email 이메일 성공', () => {
         return request(APP)
             .get('/auth/validation/email?value=testUserUnique@gmail.com')
             .expect(200)
     })
 
-    test('403 이메일 중복', () => {
+    test('GET 403 /email 이메일 중복', () => {
         return request(APP)
             .get(`/auth/validation/email?value=${user[0].email}`)
             .expect(403)
     })
 
-    test('403 이메일 형식 실패', () => {
+    test('GET 403 /email 이메일 형식 실패', () => {
         return request(APP)
             .get(`/auth/validation/email?value=${errUser[0].email}`)
             .expect(403)
     })
 
-    test('200 닉네임 성공', () => {
+    test('GET 200 /nickname 닉네임 성공', () => {
         return request(APP)
             .get(`/auth/validation/nickname?value=nicknameTestUnique`)
             .expect(200)
     })
 
-    test('403 닉네임 중복 실패', () => {
+    test('GET 403 /nickname 닉네임 중복 실패', () => {
         return request(APP)
             .get(`/auth/validation/nickname?value=${user[0].nickname}`)
             .expect(403)
     })
 
-    test('403 경로에러 /auth/validation/ddasf', () => {
+    test('GET 403 /ddasf 경로에러 ', () => {
         return request(APP)
             .get('/auth/validation/ddasf')
             .expect(404)
     })
 })
 
-describe("POST /auth/signup", () => {
+describe("POST /auth/signup 회원가입", () => {
     afterAll(async ()=>{
         testSession = session(APP);
 
@@ -130,7 +130,7 @@ describe("POST /auth/signup", () => {
         testSession = null;
     })
 
-    test('200 회원가입 성공', async () => {
+    test('POST 200 / 회원가입 성공', async () => {
         await request(APP)
             .post('/auth/signup')
             .send({
@@ -142,7 +142,7 @@ describe("POST /auth/signup", () => {
             .expect(200)
     })
 
-    test('403 회원가입 실패', async() => {
+    test('POST 403 / 회원가입 실패', async() => {
         await request(APP)
             .post('/auth/signup')
             .send({
