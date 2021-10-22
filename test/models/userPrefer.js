52ffa1547b39aac72ac52076e4685047e1a32451
FonDitbul <niki7084@naver.com>
2021-10-22 18:41:01 +0900

    prefer 데이터 수정

--- test/models/userPrefer.js
+++ test/models/userPrefer.js
@@ -1,31 +1,47 @@
-const prefer = {
-    "prefer":[
-        {
-            "category": "themes",
-            "id": 21,
-            "name": "Survival"
-        },
-        {
-            "category":"themes",
-            "id":38,
-            "name":"Open world"
-        },
-        {
-            "category":"genres",
-            "id":15,
-            "name":"Strategy"
-        },
-        {
-            "category":"genres",
-            "id":12,
-            "name":"Role-playing (RPG)"
-        },
-        {
-            "category":"genres",
-            "id":31,
-            "name":"Adventure"
-        }
-    ]
-}
+const prefer = [
+    {
+        "category": "themes",
+        "id": 21,
+        "name": "Survival"
+    },
+    {
+        "category":"themes",
+        "id":38,
+        "name":"Open world"
+    },
+    {
+        "category":"genres",
+        "id":15,
+        "name":"Strategy"
+    },
+    {
+        "category":"genres",
+        "id":12,
+        "name":"Role-playing (RPG)"
+    },
+    {
+        "category":"genres",
+        "id":31,
+        "name":"Adventure"
+    }
+]
 
-module.exports = prefer;
+
+const preferOne = [
+    {
+        "category": "themes",
+        "id": 21,
+        "name": "Survival"
+    },
+    {
+        "category":"genres",
+        "id":31,
+        "name":"Adventure"
+    }
+]
+
+
+module.exports = {
+    prefer,
+    preferOne
+};
